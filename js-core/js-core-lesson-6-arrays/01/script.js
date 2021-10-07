49cbaa8f0282da4d2f05f6497dcafc6a03e6f997
Sergey Makhnatkin <sergey.makhnatkin@gmail.com>
2021-10-08 00:17:22 +0300

    Added lesson-7

--- js-core/js-core-lesson-6-arrays/01/script.js
+++ js-core/js-core-lesson-6-arrays/01/script.js
@@ -210,3 +210,277 @@ movies.map(({ year }) => {
 
 // ...
 // callback(item) // вызов
+
+
+// const callback = (item) => {
+// 	return true;
+// } 
+// // true | false?
+// movies.filter(callback) // ?
+
+const numbers = [12, null, 13, null, null];	// [12, 13]
+
+numbers.filter(item => item !== null);
+numbers.sort((current, next) => current > next);
+
+
+
+
+
+
+
+// Давайте создадим объект фильм
+const movie = {
+	id: 'esdff-445gf-123123-fdfgs', // uniq generated hash id
+	title: 'Властелин колец',
+	type: 'фантастика',
+	year: 2000,
+	duration: 120,
+	director: 'Питер Джексон',
+	actors: ['Первый', 'Второй']
+};
+
+
+
+
+
+
+
+
+
+// Давайте создадим массив фильмов
+const movies = [{
+	id: 'esdff-445gf-123123-fdfgs', // uniq generated hash id
+	title: 'Властелин колец',
+	type: 'фантастика',
+	year: 2000,
+	duration: 120,
+	director: 'Питер Джексон',
+	actors: ['Первый', 'Второй']
+}, {
+	id: 'sdgsfd-23424-dfgdfgdfg-123123', // uniq generated hash id
+	title: 'Властелин колец 3',
+	type: 'фантастика',
+	year: 2004,
+	duration: 125,
+	director: 'Питер Джексон',
+	actors: ['Первый', 'Второй']
+}, {
+	id: 'fhjfh-sdfsf-123cfgdf-123sdfg', // uniq generated hash id
+	title: 'Властелин колец 2',
+	type: 'фантастика',
+	year: 2002,
+	duration: 122,
+	director: 'Питер Джексон',
+	actors: ['sdfgsfd-123123-sdfsdf-1231', 'sdfgsfd-123123-sdfsdf-1231']
+}];
+
+
+const actors = [{
+	id: 'sdfgsfd-123123-sdfsdf-1231',
+	name: '',
+	movies: ['sdfsfsdfs-sdfsdfs-123123-1231', 'sdfsfsdfs-sdfsdfs-123123-1231']
+}, {
+	id: 'sdfgsfd-123123-sdfsdf-1231',
+	name: '',
+	movies: ['sdfsfsdfs-sdfsdfs-123123-1231', 'sdfsfsdfs-sdfsdfs-123123-1231']
+}]
+
+const actors = {
+	'sdfgsfd-123123-sdfsdf-1231': {
+		id: 'sdfgsfd-123123-sdfsdf-1231',
+		name: '',
+		movies: ['sdfsfsdfs-sdfsdfs-123123-1231', 'sdfsfsdfs-sdfsdfs-123123-1231']
+	}, 
+	'sdfgsfd-123123-sdfsdf-1231': {
+		id: 'sdfgsfd-123123-sdfsdf-1231',
+		name: '',
+		movies: ['sdfsfsdfs-sdfsdfs-123123-1231', 'sdfsfsdfs-sdfsdfs-123123-1231']
+	}
+}
+
+
+
+// user: {
+// 	name: 'Сергей',
+// 	surname: 'Махнаткин'
+// },
+
+const userProfile = {
+	id: 'asfsdf-213sfg-sgsg-123123',
+	// name: 'Сергей Махнаткин',
+
+	user: {
+		name: 'Сергей',
+		surname: 'Махнаткин'
+	},
+
+	subscribeType: 'golden premium',
+	nickname: 'makhnatkin',
+	subscribeEndDate: '2021-10-07',
+};
+
+const nickname = 'name' 	
+console.log(userProfile[nickname]); // 'Сергей Махнаткин',
+
+console.log(userProfile.nickname); // 'makhnatkin'
+console.log(userProfile['nickname']); // 'makhnatkin'
+
+// нашОбъект['имяКлюча']	=> 	значениеЭтогоКлюча	
+
+
+
+
+
+// ['id', 'name', 'subscribeType', 'nickname', 'subscribeEndDate']
+const keys = Object.keys(userProfile); 
+
+
+
+// ['имяКлюча', 'имяКлюча', 'имяКлюча', 'имяКлюча', 'имяКлюча']
+// нашОбъект[['имяКлюча', 'имяКлюча', 'имяКлюча', 'имяКлюча', 'имяКлюча'][0]]	=> 	значениеЭтогоКлюча	
+
+
+
+
+let str = '';
+for (let i = 0; i < keys.length; i++) {
+
+	const isFirst = i === 0; // true | false;
+
+	const space = isFirst ? '' : ', ';
+	const key = keys[i];
+	const value = userProfile[key];
+
+	const newStr = space + value;
+
+	str += newStr;
+
+}
+// asfsdf-213sfg-sgsg-123123, Сергей Махнаткин, golden premium, makhnatkin, 2021-10-07
+
+
+
+
+
+// 1 шаг  ============
+// '',
+// i = 0, 0 < 5 ? true
+// str = 0
+
+// 2 шаг  ============
+// '0',
+// i = 1, 1 < 5 ? true
+// str = 01
+
+// 3 шаг  ============
+// '01',
+// i = 2, 2 < 5 ? true
+// str = 012
+
+// 4 шаг  ============
+// '012',
+// i = 3, 3 < 5 ? true
+// str = 0123
+
+// 5 шаг  ============
+// '0123',
+// i = 4, 4 < 5 ? true
+// str = 01234
+
+// 5 шаг  ============
+// '01234',
+// i = 5, 5 < 5 ? false
+// console.log(str)	// 01234
+
+
+
+// asfsdf-213sfg-sgsg-123123, Сергей Махнаткин, golden premium, makhnatkin, 2021-10-07
+console.log(str);
+
+
+// console.log(keys);
+
+// const values = Object.values(userProfile); 
+// console.log(values);
+
+
+// userProfile.user.name
+
+
+const users = [{
+	id: 'asfsdf-213sfg-sgsg-123123',
+	name: 'Сергей Махнаткин',
+	subscribeType: 'golden premium',
+	nickname: 'makhnatkin',
+	subscribeEndDate: '2021-10-07',
+}, {
+	id: '213sfg-sgsg-asfsdf-123123',
+	name: 'Сергей Махнаткин',
+	subscribeType: 'golden premium',
+	nickname: 'makhnatkin',
+	subscribeEndDate: '2021-10-07',
+}, {
+	id: 'asfsdf-213sfg-sgsg-123123',
+	name: 'Сергей Махнаткин',
+	subscribeType: 'golden premium',
+	nickname: 'makhnatkin',
+	subscribeEndDate: '2021-10-07',
+}];
+
+
+
+
+
+// ================ плохо
+const list = [{
+	id: '',
+	title: '',
+	users: [{}, {}]
+}, {
+	id: '',
+	title: '',
+	users: [{}]
+}, {
+	id: '',
+	title: '',
+	users: [{}, {}, {}]
+}]
+
+
+// ================ хорошо
+const list = [{
+	id: '',
+	title: '',
+	users: ['id-1', 'id-2']
+}, {
+	id: '',
+	title: '',
+	users: ['id-1', 'id-2']
+}, {
+	id: '',
+	title: '',
+	users: ['id-1', 'id-2']
+}]
+
+// hashMap
+const users = {
+	'id-1': {
+		id: 'id-1',
+		name: ''
+	}, 
+	'id-2': {
+		id: 'id-1',
+		name: ''
+	},
+	'id-2': {
+		id: 'id-1',
+		name: ''
+	}
+}
+
+const id = list[1].users[1] // 'id-2'
+console.log(users[id].name)
+
+
+
